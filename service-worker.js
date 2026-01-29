0190dea95f0f17940c67145fa2def8b9c30e3d33
shrey-space <sarmalkarshreyas@gmail.com>
2026-01-29 20:25:55 +0530

    Initial FarmerWebApp website

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'greenfarm-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './farmer_icon.png'
+];
+
+// Install SW and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate SW
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch cached files when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
